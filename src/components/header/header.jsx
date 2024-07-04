2515a37741e0d0616b678239ecc442e6514bd64a
MatheuscAmaral <matheuscamposdoamaral15@gmail.com>
2024-07-03 22:16:45 -0300

    fix: corrigindo bugs encontrados no dark mode na sidebar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,6 @@ import { FaBarsStaggered, FaLightbulb, FaRegLightbulb } from "react-icons/fa6";
 import { CloseButton, Stack } from '@chakra-ui/react'
 import { useContext } from 'react';
 import { LightContext } from '../../contexts/LightContext';
-import { useEffect } from 'react';
 
 
 export default function Header({ logo }) {
@@ -23,6 +22,7 @@ export default function Header({ logo }) {
         <div className="flex lg:hidden">
           <button
             type="button"
+            style={{ color: light ? "#131111" : "white" }}
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
@@ -75,7 +75,7 @@ export default function Header({ logo }) {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className=" fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel style={{ backgroundColor: light ? "white" : "#131111" }} className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <img
@@ -86,6 +86,7 @@ export default function Header({ logo }) {
             </a>
             <button
               type="button"
+              style={{ color: light ? "#131111" : "white" }}
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
             >
